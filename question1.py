ee484c8f21db04d22918aba6c824cd324ea8bef5
Dorothy <dorahnakslove@gmail.com>
2024-10-24 11:27:22 +0300

    python test

--- question1.py
+++ question1.py
@@ -1,14 +1,14 @@
 # Question 1(i)
 # Write a Python program to find the distance between two coordinate points (x1, y1) and (x2, y2).
 
-# import math
-# x1 = float(input("Enter x1: "))
-# y1 = float(input("Enter y1: "))
-# x2 = float(input("Enter x2: "))
-# y2 = float(input("Enter y2: "))
+import math
+x1 = float(input("Enter x1: "))
+y1 = float(input("Enter y1: "))
+x2 = float(input("Enter x2: "))
+y2 = float(input("Enter y2: "))
 
-# distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-# print(f"The distance between coordinate points ({x1}, {y1}) and ({x2}, {y2}) is {distance:.2f} ")
+distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+print(f"The distance between coordinate points ({x1}, {y1}) and ({x2}, {y2}) is {distance:.2f} ")
 
 
 
